77c1e1e7eef22298d0766d93f22e73bb4d29fd78
ruizmarcos770 <ruizmarcos770@gmail.com>
2025-08-01 08:17:52 -0300

    Update layout.tsx analytics 2.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,13 @@ import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
-import Script from 'next/script' //linea agregada
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: "ZOHAN - Importación de Insumos",
   description: "Especialistas en importación de insumos para exportación. La única empresa argentina liderada por alguien que recorre fábricas en China.",
-icons: {
-    icon: '/Favicon-zohan.png', // o favicon.ico
+  icons: {
+    icon: '/favicon-zohan.png',
   }
 }
 
@@ -21,29 +21,30 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
+          html {
+            font-family: ${GeistSans.style.fontFamily};
+            --font-sans: ${GeistSans.variable};
+            --font-mono: ${GeistMono.variable};
+          }
         `}</style>
       </head>
-       {/* Google Analytics */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-TJ407RM58J"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-TJ407RM58J');
-        `}
-      </Script>
-      
-      <body>{children}</body>
-      <body>{children}</body>
+      <body>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-TJ407RM58J"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TJ407RM58J');
+          `}
+        </Script>
+        
+        {children}
+      </body>
     </html>
   )
 }
